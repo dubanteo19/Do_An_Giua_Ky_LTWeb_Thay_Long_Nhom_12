04cd71387442d5530a72caf9cb2fb0e9e21e0212
dubanteo19 <21130444@st.hcmuaf.edu.vn>
2023-11-13 13:37:36 +0700

    Hoan thanh trang quan ly order

--- admin/javascripts/FakeDataBase.js
+++ admin/javascripts/FakeDataBase.js
@@ -1,3 +1,27 @@
+class Order {
+    constructor(id, statusId, userId, totalPrice, discount_id, paymentMethod, shipingFee, address) {
+        this.id = id;
+        this.statusId = statusId;
+        this.userId = userId;
+        this.totalPrice = totalPrice;
+        this.discount_id = discount_id;
+        this.paymentMethod = paymentMethod;
+        this.shipingFee = shipingFee;
+        this.address = address;
+        this.dateCreated = this.getToday();
+    }
+    getToday() {
+        let today = new Date();
+        let date = today.getHours() + ":" + today.getMinutes() + " " + today.getDate() + "/" + (today.getMonth() + 1) + "/" + today.getFullYear();
+        return date;
+    }
+}
+class Role {
+    constructor(id, description) {
+        this.id = id;
+        this.description = description;
+    }
+}
 class User {
     constructor(email, password, fullName, phone) {
         this.email = email;
@@ -6,24 +30,17 @@ class User {
         this.phone = phone;
         this.address = "";
         this.id = 0;
-        this.status = "Hoạt động";
+        this.statusId = "1";
     }
 }
 class Product {
-    constructor(id, catalogID, name) {
+    constructor(id, name, quantity, categoryId, blogId, statusId) {
         this.id = id;
-        this.catalog = catalogID;
         this.name = name;
-    }
-}
-class ProductDetail {
-    constructor(id, productID, blogID, statusID, unitInStock, discount) {
-        this.id = id;
-        this.productID = productID;
-        this.blogID = blogID;
-        this.statusID = statusID;
-        this.unitInStock = unitInStock;
-        this.discount = discount;
+        this.quantity = quantity;
+        this.categoryId = categoryId;
+        this.blogId = blogId;
+        this.statusId = statusId;
     }
 }
 class Status {
@@ -32,7 +49,7 @@ class Status {
         this.description = description;
     }
 }
-class Categories {
+class Category {
     constructor(id, parentCategories, name, active) {
         this.id = id;
         this.parentCategories = parentCategories;
@@ -43,9 +60,9 @@ class Categories {
 class DataBaseManager {
     statuses = [];
     users = []
-    catelogies;
-    products;
-    productDetails;
+    categories = [];
+    products = [];
+    orders = []
     constructor() {
         this.loadDataBase();
     }
@@ -53,18 +70,74 @@ class DataBaseManager {
     saveData() {
         localStorage.setItem("statuses", JSON.stringify(this.statuses));
         localStorage.setItem("users", JSON.stringify(this.users));
+        localStorage.setItem("products", JSON.stringify(this.products));
+        localStorage.setItem("categories", JSON.stringify(this.categories));
+        localStorage.setItem("orders", JSON.stringify(this.orders));
 
     }
     loadDataBase() {
         let statuses = JSON.parse(localStorage.getItem("statuses"));
         let users = JSON.parse(localStorage.getItem("users"));
-        if (statuses != null) {
-            this.statuses = statuses;
-        }
-        if (users != null) {
-            this.users = users;
-        }
+        let products = JSON.parse(localStorage.getItem("products"));
+        let categories = JSON.parse(localStorage.getItem("categories"));
+        let orders = JSON.parse(localStorage.getItem("orders"));
+        if (statuses != null) this.statuses = statuses;
+        if (users != null) this.users = users;
+        if (products != null) this.products = products;
+        if (categories != null) this.categories = categories;
     }
+    // Order manager
+    addOrder(order) {
+        for (const o of this.orders) {
+            if (o.id === order.id) return false;
+        }
+        this.orders.push(order);
+        this.saveData();
+        return true;
+    }
+    findOrder(id) {
+        for (const o of this.orders) {
+            if (o.id === id) return o;
+        }
+        return null;
+
+    }
+
+    // categories manager
+    addCategory(catelogy) {
+        for (const c of this.categories) {
+            if (c.id === catelogy.id) return false;
+        }
+        this.categories.push(catelogy);
+        this.saveData();
+        return true;
+    }
+    findCategory(id) {
+        for (const c of this.categories) {
+            if (c.id === id) return c;
+
+        }
+        return null;
+    }
+
+    // Products manager
+    addProduct(product) {
+        for (const p of this.products) {
+            if (p.id === product.id) return false;
+        }
+        this.products.push(product);
+        this.saveData();
+        return true;
+    }
+
+    findProduct(id) {
+        for (const p of this.products) {
+            if (p.id === id) return p;
+        }
+        return false;
+    }
+
+
     // Statuses manager
     findStatus(id) {
         for (const s of this.statuses) {
@@ -85,7 +158,7 @@ class DataBaseManager {
 
     /* Users manager*/
     autoSetUserId(user) {
-        let lastestUser = this.users[this.users.length -1];
+        let lastestUser = this.users[this.users.length - 1];
         console.log(lastestUser);
         if (lastestUser != null) {
             user.id = lastestUser.id + 1;
@@ -102,22 +175,62 @@ class DataBaseManager {
         this.saveData();
         return true;
     }
-    findUser(email) {
+    findUser(id) {
         for (const user of this.users) {
-            if (user.email === email)
+            if (user.id === id)
                 return user;
         }
         return null
-
+    }
+    deleteUser(id) {
+        let user = this.findUser(id);
+        if (user == null) {
+            return false;
+        }
+        console.log(this.users);
+        this.users = this.users.filter(function(item) {
+            return item.id !== user.id;
+        })
+        console.log(this.users)
+        this.saveData();
+        return true;
     }
 }
 let fakeDataBase = initDataBase()
 
 function initDataBase() {
     let fakeDataBase = new DataBaseManager();
+    let status1 = new Status("1", "Hoạt động");
+    let status2 = new Status("2", "Cấm");
+    let status3 = new Status("3", "Chưa kích hoạt");
+    let status4 = new Status("4", "Ẩn");
+    let status5 = new Status("5", "Sắp hết hàng");
+    let status6 = new Status("6", "Còn hàng");
+    let status7 = new Status("7", "Đang giao hàng");
+    let status8 = new Status("8", "Hoàn thành");
+    let status9 = new Status("9", "Đã hủy");
+    fakeDataBase.addStatus(status1);
+    fakeDataBase.addStatus(status2);
+    fakeDataBase.addStatus(status3);
+    fakeDataBase.addStatus(status4);
+    fakeDataBase.addStatus(status5);
+    fakeDataBase.addStatus(status6);
+    fakeDataBase.addStatus(status7);
+    fakeDataBase.addStatus(status8);
+    fakeDataBase.addStatus(status9);
     fakeDataBase.addUser(new User("admin@gmail.com", "admin", "admin", "0925821477"));
     fakeDataBase.addUser(new User("minh@gmail.com", "123", "Thanh Minh", "0925821477"));
     fakeDataBase.addUser(new User("huynhminh@gmail.com", "123", "Thanh Minh", "0925821477"));
-    // console.log(fakeDataBase.users[0]);
+    fakeDataBase.addUser(new User("chucngan@gmail.com", "123", "Chuc Ngan", "0925545457"));
+    fakeDataBase.addCategory(new Category("C1", "", "Gạo các loại ", 1));
+    fakeDataBase.addCategory(new Category("GD", "C1", "Gạo dẻo ", 1));
+    fakeDataBase.addCategory(new Category("C2", "", "Hạt", 1));
+    fakeDataBase.addProduct(new Product("GAO2", "GẠO LÀI MIÊN CAMPUCHIA 2", 100, "GD", "", "6"));
+    fakeDataBase.addProduct(new Product("GAO1", "GẠO LÀI MIÊN CAMPUCHIA 1", 100, "GD", "", "6"));
+    fakeDataBase.addProduct(new Product("GAO3", "GẠO TỨ QUÝ", 200, "GD", "", "6"));
+    fakeDataBase.addOrder(new Order("o1", "8", "2", 300000, "", "COD", 20000, "Dai hoc Nong Lam TPHCM"));
+    fakeDataBase.addOrder(new Order("o2", "7", "2", 250000, "", "COD", 20000, "Dai hoc Nong Lam TPHCM"));
+    fakeDataBase.addOrder(new Order("o3", "9", "3", 450000, "", "COD", 20000, "Linh Trung, Thuc Duc, TP HCM"));
+    fakeDataBase.addOrder(new Order("o4", "8", "3", 250000, "", "COD", 20000, "Phu Thai,  Tan Binh ,TP Ho Chi Minh"));
     return fakeDataBase;
 }
